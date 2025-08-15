7c394260c10ccafe8aaa609f31fd74f13cee4ceb
Jamal Rarejam <agarajamaldeen@gmail.com>
2025-08-15 08:04:04 -0400

    index

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ app.use(express.json());
 //   })
 // );
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 require("dotenv").config();
 const verifyToken = require("./jwtConfig");
